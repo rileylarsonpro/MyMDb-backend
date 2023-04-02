31a617d3c001050c28d66fedd265b191625f8dd9
rileylarsonpro <riley.larson.pro@gmail.com>
2023-04-02 10:58:24 -0600

    Updated get details function for 20+ season

--- controllers/media.controller.js
+++ controllers/media.controller.js
@@ -25,8 +25,8 @@ exports.getDetails = async (req, res) => {
         })
         let append = ""
         if (mediaType === "movie") append = "&append_to_response=release_dates";
-        if( mediaType === "tv") append = "&append_to_response=content_ratings";
-        const { data } = await axios.get(`${process.env.TMDB_API_HOST}/${mediaType}/${id}?api_key=${process.env.TMDB_API_KEY}&language=en-US${append}`)
+        if (mediaType === "tv") append = "&append_to_response=content_ratings";
+        let { data } = await axios.get(`${process.env.TMDB_API_HOST}/${mediaType}/${id}?api_key=${process.env.TMDB_API_KEY}&language=en-US${append}`)
         // format content ratings for us only 
         if (mediaType === "tv") {
             data.content_ratings.results = data.content_ratings.results.filter(rating => rating.iso_3166_1 === "US")
@@ -35,13 +35,43 @@ exports.getDetails = async (req, res) => {
             } else {
                 data.content_rating = data.content_ratings.results[0].rating || ""
             }
+            // for groups of 20 or less seasons make an api call with the season number appended to the end of the url
+            if (data.seasons.length > 20) {
+                let axiosCalls = []
+                for (let i = 0; i < data.seasons.length; i += 20) {
+                    let append = "&append_to_response=season/" + data.seasons[i].season_number
+                    for (let j = i + 1; (j < i + 20 && j < data.seasons.length); j++) {
+                        append += ",season/" + data.seasons[j].season_number
+                    }
+                    axiosCalls.push(axios.get(`${process.env.TMDB_API_HOST}/${mediaType}/${id}?api_key=${process.env.TMDB_API_KEY}&language=en-US${append}`))
+                }
+                const requestGroup = await axios.all(axiosCalls)
+                requestGroup.forEach(response => {
+                    data = { ...data, ...response.data }
+                })
+            }
+            else if (data.seasons.length > 0) {
+                let append = "&append_to_response=season/" + data.seasons[0].season_number
+                for (let i = 1; i < data.seasons.length; i++) {
+                    append += ",season/" + data.seasons[i].season_number
+                }
+                const { data: seasonData } = await axios.get(`${process.env.TMDB_API_HOST}/${mediaType}/${id}?api_key=${process.env.TMDB_API_KEY}&language=en-US${append}`)
+                data = { ...data, ...seasonData }
+            }
+
+            
         }
         if (mediaType === "movie") {
             data.release_dates.results = data.release_dates.results.filter(rating => rating.iso_3166_1 === "US")
             if (data.release_dates.results.length === 0) {
                 data.content_rating = ""
             } else {
-                data.content_rating = data.release_dates.results[0].release_dates.find(rating => rating.certification !== "").certification || ""
+                if (data.release_dates.results[0].release_dates.length === 0) data.content_rating = ""
+                else {
+                    let rating = data.release_dates.results[0].release_dates.find(rating => rating.certification !== "")
+                    if (!rating) data.content_rating = ""
+                    else data.content_rating = rating.certification
+                }
             }
         }
         return res.status(200).send(data)
